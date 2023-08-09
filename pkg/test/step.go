2d483569a93b1cee2afe8a084ad79f44392aa466
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-08-09 23:16:24 +0530

    Check Yaml intendation + Verify if the file exist
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/test/step.go
+++ pkg/test/step.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
 	"testing"
 	"time"
 
+	"gopkg.in/yaml.v2"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -561,6 +563,10 @@ func (s *Step) LoadYAML(file string) error {
 	for _, apply := range s.Apply {
 		if apply.object.GetObjectKind().GroupVersionKind().Kind == "TestStep" {
 			if testStep, ok := apply.object.(*harness.TestStep); ok {
+				// Validate TestStep
+				if err := validateTestStep(testStep, s.Dir); err != nil {
+					return fmt.Errorf("failed to validate TestStep object from %s: %v", file, err)
+				}
 				if s.Step != nil {
 					return fmt.Errorf("more than 1 TestStep not allowed in step %q", s.Name)
 				}
@@ -687,4 +693,58 @@ func hasTimeoutErr(err []error) bool {
 		}
 	}
 	return false
+}
+
+func validateTestStep(ts *harness.TestStep, baseDir string) error {
+	if len(ts.Apply) == 0 {
+		return fmt.Errorf("the 'Apply' field is required and missing")
+	}
+
+	// Check if referenced files in Apply exist
+	for _, apply := range ts.Apply {
+		path := filepath.Join(baseDir, apply.File)
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			return fmt.Errorf("referenced file in Apply does not exist: %s", path)
+		}
+		if err := checkYAMLError(path); err != nil {
+			return err
+		}
+	}
+	// Check if referenced files in  Assert  exist
+	for _, assert := range ts.Assert {
+		path := filepath.Join(baseDir, assert)
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			return fmt.Errorf("referenced file in Assert does not exist: %s", path)
+		}
+		if err := checkYAMLError(path); err != nil {
+			return err
+		}
+	}
+	// Check if referenced files in  Error exist
+	for _, errorPath := range ts.Error {
+		path := filepath.Join(baseDir, errorPath)
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			return fmt.Errorf("referenced file in Error does not exist: %s", path)
+		}
+		if err := checkYAMLError(path); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// checkYAMLError checks for YAML structural or indentation errors in the given file path.
+func checkYAMLError(path string) error {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("error reading file %s: %v", path, err)
+	}
+
+	var data map[string]interface{}
+	if err := yaml.Unmarshal(content, &data); err != nil {
+		return fmt.Errorf("YAML indentation or structural error in file %s: %v", path, err)
+	}
+
+	return nil
 }
