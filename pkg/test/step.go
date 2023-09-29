2b1d4dfe2867919f304e48c313cf8b1ed7e77e51
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-09-29 20:13:39 +0530

    fixes
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/test/step.go
+++ pkg/test/step.go
@@ -436,15 +436,7 @@ func metaTypeMatches(assertArray harness.AssertArray, obj client.Object) bool {
 	unstructuredObj := obj.(*unstructured.Unstructured).Object
 
 	if assertArray.Match != nil {
-		if err := testutils.IsSubset(assertArray.Match.ObjectMeta, unstructuredObj["metadata"], "/", testutils.DefaultStrategyFactory()); err != nil {
-			return false
-		}
-
-		if err := testutils.IsSubset(assertArray.Match.TypeMeta.Kind, unstructuredObj["kind"], "/", testutils.DefaultStrategyFactory()); err != nil {
-			return false
-		}
-
-		if err := testutils.IsSubset(assertArray.Match.TypeMeta.APIVersion, unstructuredObj["apiVersion"], "/", testutils.DefaultStrategyFactory()); err != nil {
+		if err := testutils.IsSubset(assertArray.Match, unstructuredObj, "/", testutils.DefaultStrategyFactory()); err != nil {
 			return false
 		}
 	}
